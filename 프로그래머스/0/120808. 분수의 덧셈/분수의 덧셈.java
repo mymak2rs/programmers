3f5a0036a157307f21d264a62a08465dd83aac7d
Junki Lee <116767717+mymak2rs@users.noreply.github.com>
2024-04-13 18:00:14 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.02 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        int numer = numer1 * denom2 + numer2 * denom1;
+        int denom = denom1 * denom2;
+
+        // 최대공약수 구하기
+        int gcd = gcd(numer, denom);
+
+        numer /= gcd;
+        denom /= gcd;
+
+        return new int[]{numer, denom};
+    }
+
+    // 최대공약수 계산
+    public int gcd(int a, int b) {
+        while (b != 0) {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+}
