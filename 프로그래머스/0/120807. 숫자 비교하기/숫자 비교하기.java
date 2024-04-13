6c7cb3076a0b9a4630272716c1d2f3eb704857d7
Junki Lee <116767717+mymak2rs@users.noreply.github.com>
2024-04-13 17:30:33 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        boolean check = num1==num2;
+        int answer = -1;
+        if(check){
+            answer = 1;
+        }
+
+        return answer;
+    }
+}
